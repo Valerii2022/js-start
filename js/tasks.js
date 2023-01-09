5e9c8823694f979537b7e5e93ce04b84c8ca375a
Valerii Pometun <vmpometun@gmail.com>
2023-01-09 21:54:45 +0200

    done till segments, start task#2

---
+++ js/tasks.js
@@ -0,0 +1,31 @@
+/*                          #1
+
+ * Напиши скрипт который просит пользователя ввести число и степень,
+ * возводит число в эту степень и выводит результат в консоль
+ */
+
+// let enterNumber = prompt('Enter number');
+// enterNumber = Number(enterNumber);
+// console.log(enterNumber);
+// let enterPow = prompt('Enter pow');
+// enterPow = Number(enterPow);
+// console.log(enterPow);
+// const powNumber = (Math.pow(enterNumber, enterPow));
+// console.log(`Result ${powNumber}`);
+// const result = enterNumber ** enterPow;
+// console.log(`Result ${result}`);
+
+/*                        #2
+
+ * Напиши скрипт который проверяет вхождение числа
+ * в отрезок обозначенный x1 и x2.
+ *
+ * - До x1
+ * - После x2
+ * - От x1 до x2
+ * - До x1 или после x2
+ */
+
+const x1 = 10;
+const x2 = 30;
+const number = 45;
