098e84e941cb5c1c2c0b74a47ffb0ee8e21bcd36
Valerii Pometun <vmpometun@gmail.com>
2023-01-09 14:42:54 +0200

    add start files

---
+++ js/vars.js
@@ -0,0 +1,13 @@
+// const age = 1000;
+// const totalPrice = 200.74;
+// const userName = 'Chelsy';
+// const message = 'Добро пожаловать!';
+// const isOpen = true;
+// const shouldConfirm = false;
+
+// console.log(age);
+
+
+// const type = typeof isOpen;
+
+// console.log(type);
