8829428d6ccca0f30d5518e3383d90a9ddb3d5a3
Valerii Pometun <vmpometun@gmail.com>
2023-01-10 19:39:29 +0200

    done modul#1

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+// function sayHi() {
+//   console.log('Hello, this is my first function!');
+// }
+// sayHi();
+
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+//   return a + b + c;
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+// function makeMessage(name, price) {
+//   const message = `You picked ${name}, price per item is ${price} credits`;
+//   return message;
+// }
+
+// console.log(makeMessage('Nick', 700));
