19061fb3354348b9e75c7545539d4b8cd08459dd
Valerii Pometun <vmpometun@gmail.com>
2023-01-12 23:46:10 +0200

    homework #1 & #2 done + training work

--- js/functions.js
+++ js/functions.js
@@ -62,15 +62,94 @@
 // }
 // console.log(word);
 
-function filterArray(numbers, value) {
-  for (let i = 0; i <= numbers.length - 1; i++) {
-    console.log(numbers[i]);
-    //  if (numbers[i] <= value) {
-    //    numbers.splice(numbers[i]);
-    //  } else {
-    //    numbers.push(numbers[i]);
-    //  }
-  }
-  return numbers;
-}
-console.log(filterArray([6, 7, 8, 10], 7));
+// function filterArray(numbers, value) {
+//   let array = [];
+//   for (let i = 0; i <= numbers.length - 1; i++) {
+//     if (numbers[i] > value) {
+//       array.push(numbers[i]);
+//     }
+//   }
+//   return array;
+// }
+// console.log(filterArray([18, 6, 7, 8, 10], 7));
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+// function getCommonElements(array1, array2) {
+//   let array = [];
+//   for (let i = 0; i <= array1.length - 1; i++) {
+//     if (array2.includes(array1[i])) {
+//       array.push(array1[i]);
+//     }
+//   }
+//   return array;
+//   console.log(array);
+// }
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+
+// const planets = ['Earth', 'Mars', 'Venus'];
+
+// for (const value of planets) {
+//   console.log(value);
+// }
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   for (const price of order) {
+//     total += price;
+//   }
+//   return total;
+// }
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+// function calculateTotalPrice2(order) {
+//   let total = 0;
+//   for (let i = 0; i < order.length; i += 1) {
+//     total += order[i];
+//   }
+//   return total;
+// }
+// console.log(calculateTotalPrice2([412, 371, 94, 63, 176]));
+
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+
+//   for (const number of numbers) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+
+//   return filteredNumbers;
+// }
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+// function getEvenNumbers(start, end) {
+//   let array = [];
+//   for (let i = start; i <= end; i += 1) {
+//     console.log(i);
+//     if (i % 2 === 0) {
+//       array.push(i);
+//     }
+//   }
+//   return array;
+// }
+// console.log(getEvenNumbers(6, 8));
+
+// function includes(array, value) {
+//   let result;
+//   for (let i = 0; i <= array.length - 1; i++) {
+//     if (array[i] === value) {
+//       result = true;
+//       return true;
+//     } else {
+//       result = false;
+//     }
+//   }
+//   return result;
+// }
+// console.log(includes([7, 5, 76, 9], 77));
---
+++ js/training.js
@@ -0,0 +1,19 @@
+// function totalPayment(total) {
+//   let totalPrice = total;
+//   let array = [];
+//   let x;
+//   let shopper1;
+//   let shopper2;
+//   let shopper3;
+//   x = totalPrice / 31;
+//   shopper1 = x;
+//   shopper2 = x * 5;
+//   shopper3 = x * 25;
+//   array = [
+//     Number(Math.round(shopper1 * 100) / 100),
+//     Number(Math.round(shopper2 * 100) / 100),
+//     Number(Math.round(shopper3 * 100) / 100),
+//   ];
+//   return array;
+// }
+// console.log(totalPayment(319.31));
