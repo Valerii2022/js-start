9918409c1b7061add904d5ca7ceeec84c6480c62
Valerii Pometun <vmpometun@gmail.com>
2023-01-16 15:05:21 +0200

    mod#3 training

--- js/objects.js
+++ js/objects.js
@@ -41,11 +41,144 @@
 // };
 // console.log(apartment);
 
-const emailInputName = 'email';
-const passwordInputName = 'password';
+// const emailInputName = 'email';
+// const passwordInputName = 'password';
 
-const credentials = {
-  [emailInputName]: 'henry.carter@aptmail.com',
-  [passwordInputName]: 'jqueryismyjam',
-};
-console.log(credentials);
+// const credentials = {
+//   [emailInputName]: 'henry.carter@aptmail.com',
+//   [passwordInputName]: 'jqueryismyjam',
+// };
+// console.log(credentials);
+
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// for (const key in apartment) {
+//   console.log(key);
+//   console.log(apartment[key]);
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+// console.log(keys);
+// console.log(values);
+
+
+// const book = {
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+
+// for (const key of keys) {
+//   console.log(key);
+//   console.log(book[key]);
+// }
+
+// function countProps(object) {
+//     let propCount = 0;
+    
+//     const keys = Object.keys(object);
+//     propCount = keys.length;
+
+
+// //   for (const key in object) {
+// //       if (object.hasOwnProperty(key)) {
+// //           console.log(key);
+// //       propCount += 1;
+// //     }
+// //   }
+
+//   return propCount;
+// }
+// console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
+
+// function countTotalSalary(salaries) {
+//     let totalSalary = 0;
+    
+//     const value = Object.values(salaries);
+//     for (const key of value) {
+//         totalSalary += key;
+//     }
+//   return totalSalary;
+// }
+
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+
+// for (const color of colors){
+//     hexColors.push(color.hex);
+//     rgbColors.push(color.rgb)
+// }
+
+// console.log(hexColors);
+// console.log(rgbColors);
+
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+//   { name: "Droid", price: 400, quantity: 7 },
+  
+// ];
+
+// function getProductPrice(productName) {
+// let price;
+//     for (const product of products) {
+//         if (productName === product.name) {
+//             price = product.price;
+//             return price;
+//         } else {
+//             price = null;
+//         }
+        
+//     }
+// return price;
+    
+// }
+// console.log(getProductPrice("Engine"));
+// console.log(getProductPrice("Radar"));
+// console.log(getProductPrice("Scanner"));
+// console.log(getProductPrice("Droid"));
+// console.log(getProductPrice("Grip"));
+
+
+/*Напиши функцію getAllPropValues(propName), яка приймає один параметр propName - ім'я (ключ) властивості. Функція повинна повернути масив всіх значень властивості з таким ім'ям з кожного об'єкта в масиві products. Якщо в об'єктах відсутні властивості з таким ім'ям, функція повинна повернути порожній масив.*/
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  // Change code below this line
+    let result = [];
+    for (const product of products) {
+        console.log(product);
+    }
+
+
+
+  // Change code above this line
+}
+
+console.log(getAllPropValues("name"));
+console.log(getAllPropValues("quantity"));
+console.log(getAllPropValues("price"));
+console.log(getAllPropValues("category"));
