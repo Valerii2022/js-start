530281b7c51969ac383bad398ee97145c918e669
Valerii Pometun <vmpometun@gmail.com>
2023-02-13 09:15:24 +0200

    practice

--- js/vars.js
+++ js/vars.js
@@ -1,82 +1,4 @@
-// let age = 1000;
-// const totalPrice = 200.74;
-// const userName = 'Ahmed';
-// const message = 'Добро пожаловать!';
-// const isOpen = true;
-// const shouldConfirm = false;
-// console.log(age);
-// const age = "55";
-// const type = typeof isOpen;
-// console.log(type);
-// console.log(age);
-// alert();
-// console.log(typeof age);
-// console.log(typeof userName);
-// console.log(typeof isOpen);
 
-// const x = 2;
-// const y = 2;
-// let z = 7;
-// z += 25;
-// console.log(z);
-// let value;
-// value = x % y;
-// console.log(value);
-// console.log(typeof value);
-// console.log(x * y);
-// console.log(x + y);
-// console.log(x - y);
-// console.log(x / y);
-// console.log(x % y);
-
-// const a = 2;
-// const b = 2;
-// const c = "2";
-// console.log(a === b);
-// console.log(c == b);
-// console.log(c === b);
-// console.log(a != c);
-// console.log(a !== c);
-
-// let value = "5";
-// console.log(value);
-// console.log(typeof value);
-// console.log(typeof Number(value));
-// value = Number(value);
-// console.log(value);
-// console.log(typeof value);
-// value = "15.55jhgfd";
-// console.log(Number.parseInt(value));
-// console.log(Number.parseFloat(value));
-// value = "56";
-// console.log(value);
-// console.log(typeof value);
-// console.log(Number(value));
-// console.log(typeof Number(value));
-// console.log(Number.isNaN(value));
-// const invalidNumber = Number("qweqwe"); // NaN
-// console.log(invalidNumber);
-// console.log(Number.isNaN(invalidNumber)); // true
-
-// console.log(0.2 + 0.3);
-// console.log(0.1 + 0.1);
-// console.log(0.1 + 0.2);
-// console.log(Number((0.13 + 0.23).toFixed(2)));
-// console.log((0.14 + 0.24).toFixed(2));
-// console.log(0.17 + 0.24);
-// console.log((0.17 * 100 + 0.24 * 100) / 100);
-
-// console.log(Math.random()); //[0 - 1)
-// let result = Number((Math.random() * (100 - 1) + 1).toFixed(0));
-// console.log(result);
-// result = Math.round(Math.random() * (80 - 20) + 20);
-// console.log(result);
-// console.log(Math.max(10, 4, 8, 45, 17));
-// console.log(Math.min(10, 4, 8, 45, 17));
-// console.log(Math.floor(1.2));
-// console.log(Math.ceil(1.2));
-// console.log(Math.pow(2, 3));
-// console.log(Math.round(1.5));
 
 // const colors = ['tomato', 'teal', 'orange', 'deeppink', 'skyblue'];
 // const max = colors.length - 1;
@@ -86,38 +8,15 @@
 // console.log(color);
 // document.body.style.backgroundColor = color;
 
-// const passenger = "Nick";
-// const seatNumber = 258;
-// const welcomeText = `Passenger ${passenger} have seat number ${seatNumber}!`;
-// console.log(welcomeText);
-// console.log(welcomeText.length);
-// console.log(welcomeText.toLowerCase());
-// console.log(welcomeText.toUpperCase());
-// console.log(welcomeText.indexOf("stay"));
-// console.log(welcomeText.indexOf("seat"));
-// console.log(welcomeText.includes("stay"));
-// console.log(welcomeText.includes("seat"));
-// console.log(welcomeText.slice(0, 5));
-// console.log(welcomeText.slice(10, 15));
-// console.log(welcomeText.slice(10, welcomeText.length));
 
-// const jsFileName = "script.js";
-// console.log(jsFileName.endsWith(".js"));
-// console.log(jsFileName.endsWith(".css"));
-// const filesName = "script.js, styles.css, index.html, modal.js";
-// const minFileName = filesName.replace(".js", ".min.js");
-// console.log(minFileName);
-// const minFilesName = filesName.replaceAll(".js", ".min.js");
-// console.log(minFilesName);
-// const allMinFilesName = filesName.replaceAll(".", ".min.");
-// console.log(allMinFilesName);
 
-// const a = 5;
-// const b = 7;
-// const c = 12;
-// console.log(b > a && c > a);
-// console.log(7 > 5 && 12 > 5);
-// console.log(b < a && c > a);
+
+
+const a = 5;
+const b = 7;
+const c = 12;
+
+console.log(b < a && c > a);
 // console.log(7 < 5 && 12 > 5);
 // console.log(b > a || c > a);
 // console.log(7 > 5 || 12 > 5);
